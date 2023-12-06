69668ed6d0590cdba4c867ba480f1f6a4029dadc
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-12-06 08:05:51 -0500

    Nice-U

--- src/Lab8Task6.java
+++ src/Lab8Task6.java
@@ -5,8 +5,11 @@ public class Lab8Task6 {
         Scanner scan = new Scanner(System.in);
         String msg = "";
         boolean done = false;
+        String leftSpaces = "";
+        String rightSpaces = "";
 
         do{
+            System.out.println("Please enter a message to be centered.");
             msg = scan.nextLine();
 
             if(msg.length()<54){
@@ -21,15 +24,28 @@ public class Lab8Task6 {
 
     public static void getPrettyHeader(String msg) {
         boolean done = false;
-        int z = (54-msg.length()/2);
-        for (int i = 0; i != 60; i++) {
+        int leftSpaces = ((54-msg.length())/2);
+        int rightSpaces = (54-leftSpaces-msg.length());
+        int a = 0;
+        for (int i = 0; i < 61; i++) {
             System.out.print("*");
         }
         System.out.println();
-        for (int j = 0; j <=54;j++) {
+        for (int m = 0; m < 61;m++) {
+            if (m<3||m>57) {
+                System.out.print("*");
+            }else if ( m > leftSpaces + 3 && m< 57- leftSpaces){
+                System.out.print(msg.substring(a, a+1));
+                a++;
+            } else{
+                System.out.print(" ");
+            }
         }
+
+        System.out.print(msg);
+
         System.out.println();
-        for (int i = 0; i != 60; i++) {
+        for (int i = 0; i < 61; i++) {
             System.out.print("*");
         }
     }
